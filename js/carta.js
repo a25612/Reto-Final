a43af96b1430ecb050369d6f4d2a1e30aa63f745
Aleexalagoon <a27304@svalero.com>
2024-05-09 20:07:17 +0200

    añadido todos los productos y boton de eliminar

--- js/carta.js
+++ js/carta.js
@@ -1,3 +1,4 @@
+
 var carrito = []; // Array para almacenar los productos
 var total = 0; // Variable para almacenar el total del carrito
 
@@ -13,7 +14,7 @@ function mostrarCarrito() {
 
 // Función para agregar un producto al carrito
 function addToCart(nombre, precio) {
-    carrito.push({nombre: nombre, precio: precio});
+    carrito.push({ nombre: nombre, precio: precio });
     total += precio;
     renderCarrito();
     mostrarCarrito();
@@ -31,11 +32,28 @@ function renderCarrito() {
     carrito.forEach(function(producto) {
         var listItem = document.createElement('li');
         listItem.textContent = producto.nombre + ' - $' + producto.precio;
+        
+        var buttonLess = document.createElement('button');
+        buttonLess.textContent = '-';
+        buttonLess.onclick = function() {
+            removeFromCart(producto); // Llama a la función para eliminar el producto específico
+        };
+        
+        listItem.appendChild(buttonLess);
         listaCarrito.appendChild(listItem);
     });
     
     // Mostrar el total del carrito
     totalCarrito.textContent = '$' + total.toFixed(2);
+}
+function removeFromCart(producto) {
+    var index = carrito.indexOf(producto); // Obtiene el índice del producto en el carrito
+    if (index !== -1) {
+        total -= producto.precio; // Restar el precio del producto al total
+        carrito.splice(index, 1); // Eliminar el producto del carrito
+        renderCarrito(); // Volver a renderizar el carrito
+        mostrarCarrito(); // Mostrar el carrito si hay productos en él
+    }
 }
 
 // Función para vaciar el carrito
@@ -44,4 +62,17 @@ function vaciarCarrito() {
     total = 0;
     renderCarrito();
     mostrarCarrito();
+}
+
+var carritoVisible = true; // Variable para controlar la visibilidad del carrito
+
+// Función para alternar la visibilidad del carrito
+function toggleCarrito() {
+    var carritoCampo = document.getElementById('carritoCampo');
+    if (carritoVisible) {
+        carritoCampo.style.display = 'none'; // Ocultar el carrito
+    } else {
+        carritoCampo.style.display = 'block'; // Mostrar el carrito
+    }
+    carritoVisible = !carritoVisible; // Cambiar el estado de la variable
 }
